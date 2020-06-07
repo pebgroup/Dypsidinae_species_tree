dfb9b399189b865006552ed7df2a1476c67f4344
Wolf Eiserhardt <wolf.eiserhardt@bios.au.dk>
2020-06-07 20:10:52 +0200

    tree plotting update

--- trees_and_plots/tree_visuals.R
+++ trees_and_plots/tree_visuals.R
@@ -1,68 +1,94 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(geiger)
-library(ape)
 library(phytools)
+library(colorspace)
+library(ggplot2)
+#library(BiocManager)
+library(vctrs)
+library(ggtree)
 
-tree_optrimal <- read.tree("astral_tree_prelim_renamed.tre")
+# Load trees
+tree_optrimal <- read.tree("trees/astral_tree_prelim_renamed.tre")
 tree_optrimal$edge.length[is.na(tree_optrimal$edge.length)] <- 1
-tree_optrimal <- root(tree_optrimal, node=getMRCA(tree_optrimal, c("Loxococcus-rupicola-SBL234-S35", "Loxococcus-rupicola-SBL8-S7")))
+tree_optrimal <- ape::root(tree_optrimal, node=getMRCA(tree_optrimal, c("Loxococcus-rupicola-SBL234-S35", "Loxococcus-rupicola-SBL8-S7")), edgelabel = TRUE)
 
-tree_gt0.5 <- read.tree("astral_tree_prelim_gt0.5_renamed.tre")
+tree_gt0.5 <- read.tree("trees/astral_tree_prelim_gt0.5_renamed.tre")
 tree_gt0.5$edge.length[is.na(tree_gt0.5$edge.length)] <- 1
-tree_gt0.5 <- root(tree_gt0.5, node=getMRCA(tree_gt0.5, c("Loxococcus-rupicola-SBL234-S35", "Loxococcus-rupicola-SBL8-S7")))
+tree_gt0.5 <- ape::root(tree_gt0.5, node=getMRCA(tree_gt0.5, c("Loxococcus-rupicola-SBL234-S35", "Loxococcus-rupicola-SBL8-S7")), edgelabel = TRUE)
 
+# Load gene tree stats (number of gene trees per species) and PoM groups
+gtstats <- read.csv("genetrees_per_taxon_prelim.csv",sep=";")
+gtstats <- gtstats[!is.na(gtstats$No.of.genes) & gtstats$Tip.label %in% tree_optrimal$tip.label,]
+nogt <- gtstats$No.of.genes
+names(nogt) <- gtstats$Tip.label
+
+# Define twelve categories for colour scale
+breaks <- seq(from=min(nogt), to=max(nogt), length.out = 13)
+breaks[1] <- -Inf
+breaks[13]  <- Inf
+
+nogt_class <- cut(nogt, breaks = breaks, labels=1:12)
+
+# Define colours
+nogt_col <- diverge_hcl(12,h=c(255,330),l=c(40,90))[as.numeric(as.vector(nogt_class))]
+names(nogt_col) <- gtstats$Tip.label
+
+
+# Plot cophyloplot
 association <- cbind(tree_optrimal$tip.label, tree_optrimal$tip.label)
 
 coplot <- cophylo(tree_optrimal, tree_gt0.5, assoc=association, rotate=TRUE)
 
-png(filename="cophyloplot2.png", width=3500, height=3500)
+png(filename="plots/cophyloplot3.png", width=3500, height=3500)
 
 cophyloplot(coplot[[1]][[1]], coplot[[1]][[2]], assoc=coplot[[2]], use.edge.length = FALSE, space = 200,
             length.line = 30, gap = 3, type = "phylogram", rotate = FALSE,
-            col = par("fg"), lwd = par("lwd"), lty = par("lty"),
+            col=nogt_col[association[,1]], lwd = par("lwd"), lty = par("lty"),
             show.tip.label = TRUE, cex=.5)  #rotate lets you rotate in plot panel
 
+breaks_for_legend_floor <- floor(seq(from=min(nogt), to=max(nogt), length.out = 13))
+breaks_for_legend_ceiling <- ceiling(seq(from=min(nogt), to=max(nogt), length.out = 13))
+leg <- c()
+for(i in 1:12){
+  leg <- c(leg, paste(breaks_for_legend_ceiling[i],"-",breaks_for_legend_floor[i+1])) 
+}
+
+legend("top", legend=leg, fill=diverge_hcl(12,h=c(255,330),l=c(40,90)), cex=2, horiz=TRUE)
+
 dev.off()
 
-#Check tree characters; is it rooted what are the tip names etc.               
-SaxifragaTree   
-str(SaxifragaTree)
-SaxifragaTree$tip.label # shows vector tip.label; tip names and tip numbers
-SaxifragaTree$Nnode
-plot(SaxifragaTree)
+# Phylo barplot
 
-#do we need to properly re-root the tree?
-findMRCA(SaxifragaTree,c("Itea_coriacea_S372", "Itea_chinensis_S371")) #find ancestral node: 615
-SaxifragaTreeRoot <- reroot(SaxifragaTree, 615, position=NULL, interactive=FALSE)
+png(filename="plots/phylobarplot.png", width=1500, height=2000)
 
-findMRCA(SaxifragaTreeTRIM,c("Itea_coriacea_S372", "Itea_chinensis_S371"))#find ancestral node: 508
-SaxifragaTreeTRIMRoot <- reroot(SaxifragaTreeTRIM, 508, position=NULL, interactive=FALSE)
-plot(SaxifragaTreeTRIMRoot)
+plotTree.barplot(tree_optrimal, nogt, scale=1000, width=10)
 
-
-
-#creation of the association matrix:
-#what does it need to look like? 
-#Any tutorial makes us set the same trees against each other, not the different trees
-#Either way the association matrix does not our absence of cross-links
-association <- cbind(SaxifragaTreeRoot$tip.label, SaxifragaTreeRoot$tip.label)
-
-
-#make cophyloplot
-png(filename="cophyloplot.png")
-cophyloplot(SaxifragaTreeRoot, SaxifragaTreeTRIMRoot, assoc = association, use.edge.length = FALSE, space = 0,
-            length.line = 30, gap = 3, type = "phylogram", rotate = FALSE,
-            col = par("fg"), lwd = par("lwd"), lty = par("lty"),
-            show.tip.label = FALSE)  #rotate lets you rotate in plot panel
 dev.off()
 
 
-####  Result: the trees have the same tips, same root, but the combination of association matrix and cophyloplot do not result in links between the same tip labels!!
 
+# PoM groups 
 
+# Create PoM group presence/absence matrix
+pomgroup <- gtstats$PoM.Group
+names(pomgroup) <- gtstats$Tip.label
+pomgroup <- pomgroup[!is.na(pomgroup)]
+heatmapData <- matrix(nrow = length(tree_optrimal$tip.label), ncol=18)
+rownames(heatmapData) <- tree_optrimal$tip.label
+heatmapData[,] <- 0
+for(i in 1:length(pomgroup)){
+  heatmapData[names(pomgroup[i]), pomgroup[i]] <- 1
+}
+rn <- rownames(heatmapData)
+heatmapData <- as.data.frame(heatmapData)
+heatmapData <- as.data.frame(sapply(heatmapData, as.character))
+rownames(heatmapData) <- rn
+colnames(heatmapData) <- paste("G",1:18,sep="")
 
+png(filename="plots/pomgroups2.png", width=3000, height=1750)
 
-##second option: cophylo function from phytoolS, but it does not plot a figure. Can be run without cophyloplot to no avail (finds copies of names automatically)
-coplot <- cophylo(SaxifragaTreeRoot, SaxifragaTreeTRIMRoot, assoc=association, rotate=TRUE)
-png(filename="cophyloplot2.png")
-plot(coplot)
+p <- ggtree(tree_optrimal, layout="rectangular") + geom_tiplab(size=3, align=TRUE, linetype='dashed', linesize=.3) + geom_text2(aes(subset = !isTip & as.numeric(label) < 1, label=label, color="red"))
+gheatmap(p, heatmapData, offset=1.5, width=.4, font.size=3) +
+  scale_fill_manual(breaks=c("0", "1"), values=c("lightgrey", "black"), name="PoM group")
+
 dev.off()
